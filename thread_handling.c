f15b6f222793294ef96af34dd71df8dad0c007bf
Pablo FAUST <pfaust@e2r2p3.42.fr>
2018-03-20 13:27:13 +0100

    thread_handling.c a la norme

--- thread_handling.c
+++ thread_handling.c
@@ -6,7 +6,7 @@
 /*   By: cvermand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 17:44:57 by cvermand          #+#    #+#             */
-/*   Updated: 2018/03/19 11:56:13 by pfaust           ###   ########.fr       */
+/*   Updated: 2018/03/20 13:26:58 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 t_screen	**create_tmp_screens(t_screen **screens)
 {
 	int		i;
-	
+
 	if (!(screens = ft_memalloc(sizeof(t_screen *) * 4)))
 		return (0);
 	i = 0;
 	while (i < 4)
 	{
 		if (!(screens[i] = malloc(sizeof(t_screen))))
-				return (0);
+			return (0);
 		i++;
 	}
 	return (screens);
@@ -40,10 +40,10 @@ t_screen	**init_tmp_screens_info(t_screen **screens, t_env *env, int nbr_scr)
 		screens[i]->height = env->screen[nbr_scr]->height;
 		screens[i]->data_addr = env->data_addr;
 		screens[i]->min_scr_x = env->screen[nbr_scr]->min_x;
-		screens[i]->min_scr_y = env->screen[nbr_scr]->min_y;	
+		screens[i]->min_scr_y = env->screen[nbr_scr]->min_y;
 		screens[i]->max_scr_x = env->screen[nbr_scr]->max_x;
-		screens[i]->max_scr_y = env->screen[nbr_scr]->max_y;	
-		screens[i]->ratio_x = env->screen[nbr_scr]->ratio_x;	
+		screens[i]->max_scr_y = env->screen[nbr_scr]->max_y;
+		screens[i]->ratio_x = env->screen[nbr_scr]->ratio_x;
 		screens[i]->ratio_y = env->screen[nbr_scr]->ratio_y;
 		screens[i]->palette = env->palette;
 		screens[i]->palettes = env->palettes;
@@ -55,27 +55,26 @@ t_screen	**init_tmp_screens_info(t_screen **screens, t_env *env, int nbr_scr)
 t_screen	**init_args(t_screen **screens, int nbr_scr, t_env *env)
 {
 	if (!(screens = create_tmp_screens(screens)))
-		return (NULL);	
-	screens = init_tmp_screens_info(screens, env, nbr_scr);		
-	init_arg_limits(env->screen[nbr_scr]->min_x , 
+		return (NULL);
+	screens = init_tmp_screens_info(screens, env, nbr_scr);
+	init_arg_limits(env->screen[nbr_scr]->min_x,
 			env->screen[nbr_scr]->min_y, screens[0]);
-	init_arg_limits(env->screen[nbr_scr]->min_x + env->screen[nbr_scr]->width / 2, 
-			env->screen[nbr_scr]->min_y , screens[1]);
-	init_arg_limits(env->screen[nbr_scr]->min_x, 
-			env->screen[nbr_scr]->min_y + env->screen[nbr_scr]->height / 2, 
-			screens[2]);	
-	init_arg_limits(env->screen[nbr_scr]->min_x + env->screen[nbr_scr]->width / 2, 
-			env->screen[nbr_scr]->min_y + env->screen[nbr_scr]->height / 2, 
-			screens[3]);
+	init_arg_limits(env->screen[nbr_scr]->min_x +
+			env->screen[nbr_scr]->width / 2,
+			env->screen[nbr_scr]->min_y, screens[1]);
+	init_arg_limits(env->screen[nbr_scr]->min_x,
+			env->screen[nbr_scr]->min_y + env->screen[nbr_scr]->height / 2,
+			screens[2]);
+	init_arg_limits(env->screen[nbr_scr]->min_x +
+			env->screen[nbr_scr]->width / 2, env->screen[nbr_scr]->min_y +
+			env->screen[nbr_scr]->height / 2, screens[3]);
 	return (screens);
 }
 
-
-void	init_arg_limits(int min_x, int min_y, t_screen *screen)
+void		init_arg_limits(int min_x, int min_y, t_screen *screen)
 {
 	screen->min_x = min_x;
 	screen->max_x = screen->min_x + screen->width / 2;
 	screen->min_y = min_y;
 	screen->max_y = screen->min_y + screen->height / 2;
 }
-
