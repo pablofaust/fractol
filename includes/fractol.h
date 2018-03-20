77840a87cf15ac11ef942aef87c406478c40477d
Pablo FAUST <pfaust@e2r1p2.42.fr>
2018-03-20 15:45:41 +0100

    fractol.h a la norme

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: cvermand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 14:11:27 by cvermand          #+#    #+#             */
-/*   Updated: 2018/03/20 14:13:44 by pfaust           ###   ########.fr       */
+/*   Updated: 2018/03/20 15:45:26 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,17 @@
 # define MOTION_NOTIFY 6
 # define KEY_PRESS 2
 # define PTR_MOTION_MASK (1L<<6)
-#define KEY_PRESS_MASK	(1L<<0)
+# define KEY_PRESS_MASK	(1L<<0)
 
-typedef struct		s_iter
+typedef struct			s_iter
 {
-	double		o_x;
-	double		o_y;
-	double		x;
-	double		y;
-}					t_iter;
+	double				o_x;
+	double				o_y;
+	double				x;
+	double				y;
+}						t_iter;
 
-struct		s_screen;
+struct s_screen;
 
 typedef struct			s_env
 {
@@ -79,15 +79,15 @@ typedef struct			s_env
 
 typedef struct			s_fractal
 {
-	double			zoom;
-	char			name;
-	int				(*f)(t_env*);
-	int				iteration;
-	double			start_x;
-	double			start_y;
-	double			const_x;
-	double			const_y;
-	unsigned int	hex;
+	double				zoom;
+	char				name;
+	int					(*f)(t_env*);
+	int					iteration;
+	double				start_x;
+	double				start_y;
+	double				const_x;
+	double				const_y;
+	unsigned int		hex;
 }						t_fractal;
 
 typedef	struct			s_screen
@@ -112,51 +112,56 @@ typedef	struct			s_screen
 	unsigned int		*data_addr;
 }						t_screen;
 
-
-int					mandelbrot(t_env *env);
-int					buddhabrot(t_env *env);
-int					antibuddhabrot(t_env *env);
-int					julia(t_env *env);
-int					init_env(t_env *env, char *av);
-void				init_mandelbrot(t_fractal *fractal);
-void				init_buddhabrot(t_fractal *fractal);
-void				init_antibuddhabrot(t_fractal *fractal);
-void				init_julia(t_fractal *fractal);
-t_screen			**init_screens(t_screen **screen, char menu_on, char *param);
-void				free_fractal(t_screen **screens);
-void				free_screens(t_screen **screens);
-void				events_listener(t_env *env);
-unsigned int		hsv_calculator(double hue, double saturation, double bright);
-unsigned int		hsl_calculator(double hue, double saturation, double light);
-unsigned int		hex_to_rgb_to_hsl(unsigned int hex);
-unsigned int		merge_two_colors(unsigned int color1, unsigned int color2);
-unsigned int		palette(int	iter);
-void				draw_circle(t_env *env);
-void				get_screen_ratio(double width, double height, t_screen *screen);
-int					get_x_min(int order);
-int					get_x_max(int order, char menu_on);
-int					get_y_min(int order);
-int					get_y_max(int order);
-t_fractal			*get_fractal(int order);
-int					get_screen_by_fractal_name(t_env *env, char c);
-t_screen			*get_screen_ptr_by_fractal_name(t_env *env, char c);
-t_screen			**init_args(t_screen **screens, int nbr_screen, t_env *env);
-void				init_arg_limits(int min_x, int min_y, t_screen *screen);
-int					key_hook(int keycode, t_env *env);
-int					loop_hook(int x, int y, t_env *env);
-int					julia_loop(int x, int y, t_env *env);
-int					mouse_hook(int button, int x, int y, t_env *env);
-int					zoom(int button, int x, int y, t_env *env);
-void				clear_image(t_env *env);
-void				color_menu(t_env *env);
-void				display_fractals(t_env *env);
-void				display_screen_one(t_env *env);
-void				clear_and_redraw(t_env *env);
-void				clear_zone(int zone, t_env *env);
-void				display_info_menu(t_env *env);
-void				toggle_info_menu(t_env *env);
-int					set_palettes(t_env *env);
-void				set_menu(t_env *env);
-void				*thread_anti(void *arg);
-void				*thread_buddha(void *arg);
+int						mandelbrot(t_env *env);
+int						buddhabrot(t_env *env);
+int						antibuddhabrot(t_env *env);
+int						julia(t_env *env);
+int						init_env(t_env *env, char *av);
+void					init_mandelbrot(t_fractal *fractal);
+void					init_buddhabrot(t_fractal *fractal);
+void					init_antibuddhabrot(t_fractal *fractal);
+void					init_julia(t_fractal *fractal);
+t_screen				**init_screens(t_screen **screen,
+							char menu_on, char *param);
+void					free_fractal(t_screen **screens);
+void					free_screens(t_screen **screens);
+void					events_listener(t_env *env);
+unsigned int			hsv_calculator(double hue, double saturation,
+							double bright);
+unsigned int			hsl_calculator(double hue, double saturation,
+							double light);
+unsigned int			hex_to_rgb_to_hsl(unsigned int hex);
+unsigned int			merge_two_colors(unsigned int color1,
+							unsigned int color2);
+unsigned int			palette(int	iter);
+void					draw_circle(t_env *env);
+void					get_screen_ratio(double width, double height,
+							t_screen *screen);
+int						get_x_min(int order);
+int						get_x_max(int order, char menu_on);
+int						get_y_min(int order);
+int						get_y_max(int order);
+t_fractal				*get_fractal(int order);
+int						get_screen_by_fractal_name(t_env *env, char c);
+t_screen				*get_screen_ptr_by_fractal_name(t_env *env, char c);
+t_screen				**init_args(t_screen **screens,
+							int nbr_screen, t_env *env);
+void					init_arg_limits(int min_x, int min_y, t_screen *screen);
+int						key_hook(int keycode, t_env *env);
+int						loop_hook(int x, int y, t_env *env);
+int						julia_loop(int x, int y, t_env *env);
+int						mouse_hook(int button, int x, int y, t_env *env);
+int						zoom(int button, int x, int y, t_env *env);
+void					clear_image(t_env *env);
+void					color_menu(t_env *env);
+void					display_fractals(t_env *env);
+void					display_screen_one(t_env *env);
+void					clear_and_redraw(t_env *env);
+void					clear_zone(int zone, t_env *env);
+void					display_info_menu(t_env *env);
+void					toggle_info_menu(t_env *env);
+int						set_palettes(t_env *env);
+void					set_menu(t_env *env);
+void					*thread_anti(void *arg);
+void					*thread_buddha(void *arg);
 #endif
